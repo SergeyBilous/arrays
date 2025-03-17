f3001c68fb98aa4b2b1533302f9176c43963c75d
Sergey Bilous <bill-gh@yandex.ru>
2025-03-17 20:32:47 +0300

    Add equals method

--- Resume.java
+++ Resume.java
@@ -10,4 +10,13 @@ public class Resume {
     public String toString() {
         return uuid;
     }
+
+    @Override
+    public boolean equals(Object another) {
+        if (!(another instanceof Resume)) {
+            return false;
+        }
+        Resume c = (Resume) another;
+        return c.uuid.equals(this.uuid);
+    }
 }
